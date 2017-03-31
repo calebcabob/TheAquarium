334d8ae0772f33d7558616c7ce6d22c614a21304
LorDClockaN <lordclockan@gmail.com>
2017-03-31 19:29:20 -0400

    OmniJaws: Statusbar Weather [2/2]
    This is a revert of the original implementation of statusbar weather by @LorDClockaN, plus adding his new statusbar weather made to work with OmniJaws!!
    
    Picked from: http://gerrit.aicp-rom.com/#/c/34683/

--- src/com/aquarios/settings/fragments/StatusBarSettings.java
+++ src/com/aquarios/settings/fragments/StatusBarSettings.java
@@ -16,8 +16,20 @@
 
 package com.aquarios.settings.fragments;
 
+import android.content.ContentResolver;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.ListPreference;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.view.View;
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+import android.support.v4.app.Fragment;
+import android.widget.LinearLayout;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
@@ -26,15 +38,52 @@ import com.android.settings.SettingsPreferenceFragment;
 
 public class StatusBarSettings extends SettingsPreferenceFragment {
 
+     private static final String PREF_STATUS_BAR_WEATHER = "status_bar_weather";
+
+     private ListPreference mStatusBarWeather;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.statusbar_settings);
         final PreferenceScreen prefScreen = getPreferenceScreen();
+
+       // Status bar weather
+       mStatusBarWeather = (ListPreference) findPreference(PREF_STATUS_BAR_WEATHER);
+       int temperatureShow = Settings.System.getIntForUser(resolver,
+               Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP, 0,
+               UserHandle.USER_CURRENT);
+       mStatusBarWeather.setValue(String.valueOf(temperatureShow));
+       if (temperatureShow == 0) {
+           mStatusBarWeather.setSummary(R.string.statusbar_weather_summary);
+       } else {
+           mStatusBarWeather.setSummary(mStatusBarWeather.getEntry());
+       }
+          mStatusBarWeather.setOnPreferenceChangeListener(this);
     }
 
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.AQUARIOS;
     }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            ContentResolver resolver = getActivity().getContentResolver()
+            if (preference == mStatusBarWeather) {
+                int temperatureShow = Integer.valueOf((String) newValue);
+                int index = mStatusBarWeather.findIndexOfValue((String) newValue);
+                Settings.System.putIntForUser(resolver,
+                        Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP,
+                        temperatureShow, UserHandle.USER_CURRENT);
+                if (temperatureShow == 0) {
+                    mStatusBarWeather.setSummary(R.string.statusbar_weather_summary);
+                } else {
+                    mStatusBarWeather.setSummary(
+                            mStatusBarWeather.getEntries()[index]);
+                }
+                return true;
+           }
+           return false;
+     }
 }
